273cc601f2f2bf39aa2e71f475f529884176b1e6
Gabriel Miguel <gmiguel14@gmail.com>
2019-10-01 11:27:25 -0300

    Create connection with mongoose

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-3xv1r.mongodb.net/omnistack?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.json);
 
