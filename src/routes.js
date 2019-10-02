46e9a3bfd009dea84f57306b02c495a437c69288
Gabriel Miguel <gmiguel14@gmail.com>
2019-10-02 15:42:03 -0300

    Create booking and Dashboard controllers

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 routes = express.Router();
 upload = multer(uploadConfig);
@@ -11,6 +13,8 @@ upload = multer(uploadConfig);
 routes.post('/sessions', SessionController.store);
 routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+routes.post('/spots/:spot_id/bookings',  SpotController.store);
+routes.post('/dashboard', BookingController.store);
 
 
 routes.get('/test', (req, res) => {
