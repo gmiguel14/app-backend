6c9607bcde16efac07933ac4f9d8e82ddef468ac
Gabriel Miguel <gmiguel14@gmail.com>
2019-10-02 14:16:32 -0300

    create models and controllers for user and spot

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
 
 routes = express.Router();
+upload = multer(uploadConfig);
 
-routes.post('users/', (req, res) => {
-  return res.json(req.body);
+routes.post('/sessions', SessionController.store);
+routes.get('/spots', SpotController.index);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+
+routes.get('/test', (req, res) => {
+  return res.json({ teste: 'teste'})
 });
 
 module.exports = routes;
